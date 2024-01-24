d2c4e4f2c745fbae07bc4218c125457290eb23ab
6awdsasdaw9 <awdsasdaw@yahoo.com>
2024-01-24 17:08:45 +0300

    Update InputService.cs

--- Assets/Scripts/Services/Input/InputService.cs
+++ Assets/Scripts/Services/Input/InputService.cs
@@ -1,58 +1,105 @@
 ﻿using System;
 using UnityEngine;
+using Utils;
 
 namespace Services
 {
     public class InputService : MonoBehaviour
     {
+        [SerializeField] private GameController _gameController;
+
         private IInputDirectionListener _mouseDirectionListener;
         private IInputDirectionListener _keyDirectionListener;
-        
+        private IInputDirectionListener _currentDirectionListener;
+
         private bool _isMouse;
+        private bool _isPlaying;
 
         private Vector2Int _direction;
-        public event Action<Vector2Int> SetNewDirectionEvent; 
+        public event Action<Vector2Int> SetNewDirectionEvent;
+
         private void Awake()
         {
             _isMouse = Screen.width < Screen.height;
-            _keyDirectionListener =  new KeyDirectionListener();
+            _keyDirectionListener = new KeyDirectionListener();
             _mouseDirectionListener = new MouseDirectionListener();
+            SubscribeToEvents(true);
         }
-        
+
         private void Update()
         {
             if (Input.anyKeyDown && _isMouse)
             {
                 _isMouse = false;
                 _keyDirectionListener.SetDirection(_mouseDirectionListener.GetDirection());
+                _currentDirectionListener = _keyDirectionListener;
             }
+
             if (Input.GetMouseButtonDown(0) && !_isMouse)
             {
                 _isMouse = true;
                 _mouseDirectionListener.SetDirection(_keyDirectionListener.GetDirection());
+                _currentDirectionListener = _mouseDirectionListener;
             }
 
-            if (_isMouse)
+            if (_gameController.GameState is GameState.Play or GameState.AwaitInput)
             {
-                _mouseDirectionListener.SetDirection();
+                if (!_isPlaying)
+                {
+                    _isPlaying = true;
+                }
+                
+                _currentDirectionListener?.SetDirection();
+
+                var dir = GetDirection();
+                if (_direction != dir)
+                {
+
+                    Debugging.Instance.Log($"Switch is playing {_isPlaying} ", Debugging.Type.Input);
+
+                    Debugging.Instance.Log($"Set new direction {dir} ", Debugging.Type.Input);
+                    _direction = dir;
+                    SetNewDirectionEvent?.Invoke(_direction);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            SubscribeToEvents(false);
+        }
+
+        public Vector2Int GetDirection()
+        {
+            return _currentDirectionListener != null && _isPlaying
+                ? _currentDirectionListener.GetDirection()
+                : Vector2Int.zero;
+        }
+
+        private void SubscribeToEvents(bool flag)
+        {
+            if (flag)
+            {
+                _gameController.EndGameEvent += OnEndGame;
+                _gameController.PauseEvent += OnPauseGame;
             }
             else
             {
-                _keyDirectionListener.SetDirection();
-            }
-
-            var dir = GetDirection();
-            if (_direction != dir)
-            {
-                _direction = dir;
-                SetNewDirectionEvent?.Invoke(_direction);
+                _gameController.EndGameEvent -= OnEndGame;
+                _gameController.PauseEvent -= OnPauseGame;
             }
         }
-        
-        public Vector2Int GetDirection()
+
+        private void OnPauseGame(bool isPause)
         {
-            return _isMouse ? _mouseDirectionListener.GetDirection() : _keyDirectionListener.GetDirection();
+            _isPlaying = !isPause;
+            Debugging.Instance.Log($"On pause -> is playing {_isPlaying} ", Debugging.Type.Input);
+        }
+
+        private void OnEndGame(bool isWon)
+        {
+            _isPlaying = false;
+            Debugging.Instance.Log($"On end game -> is playing {_isPlaying} ", Debugging.Type.Input);
         }
     }
-
 }
