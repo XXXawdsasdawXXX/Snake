7b5bac9481085daf34ddb63299a742abfbab348c
6awdsasdaw9 <awdsasdaw@yahoo.com>
2024-01-30 17:30:31 +0300

    food scaling

---
+++ Assets/Scripts/Entities/Food/FoodAnimation.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Entities
+{
+    public class FoodAnimation : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            transform.DOScale(new Vector3(1.4f,1.4f,1), 1).SetLoops(-1, LoopType.Yoyo)
+                .SetLink(gameObject, LinkBehaviour.KillOnDisable);
+        }
+    }
+}
