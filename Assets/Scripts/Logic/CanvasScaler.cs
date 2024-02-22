43576889bae490204db1487239e549911aea1bc0
6awdsasdaw9 <awdsasdaw@yahoo.com>
2024-02-22 15:35:02 +0300

    .

---
+++ Assets/Scripts/Logic/CanvasScaler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Logic
+{
+    public class CanvasScaler : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _canvas;
+        private void Awake()
+        {
+            if (!Constants.IsMobileDevice())
+            {
+                Debugging.Instance?.Log($"Camera can't set other size", Debugging.Type.Camera);
+                return;
+            }
+
+            var oldSize = _canvas.sizeDelta;
+            _canvas.sizeDelta = new Vector2(2000, oldSize.y);
+
+        }
+    }
+}
